4f61afb001950dbad52edeb5e0b0a9242eb49486
Adrian <ianxbautista06@yahoo.com>
2022-10-05 15:36:00 +0800

    commit pythonactivity1

---
+++ src/pythonactivity1.py
@@ -0,0 +1,5 @@
+firstname = input("Enter your first name: ")
+middlename = input("Enter your middle name: ")
+lastname = input("Enter your last name: ")
+
+print(f'Hello {firstname} {middlename} {lastname}')
