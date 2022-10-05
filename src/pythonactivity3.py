f79be23a6036f77a6a6823e0b24cd145374333f2
Adrian <ianxbautista06@yahoo.com>
2022-10-05 15:47:53 +0800

    commit pythonactivity3

---
+++ src/pythonactivity3.py
@@ -0,0 +1,6 @@
+number = input("Enter Number: ")
+square = int(number) * int(number)
+cube = int(number) * int(number) * int(number)
+
+print(f'The cube is {cube}')
+print(f'the square is {square}')
