3537a6b544c9699f5dd9f770951ca65e8f5ef9b2
Adrian <ianxbautista06@yahoo.com>
2022-10-05 14:58:06 +0800

    commit python2

---
+++ src/python2.py
@@ -0,0 +1,22 @@
+# variable that holds an integer
+number = 10
+# display the value
+# first way
+print("The number is " + str(number))
+# second way
+print(f'the number is {number}')
+
+# variable that holds a string
+name = "Bauts"
+# display
+print(f'the name is {name}')
+
+# variable that holds a float
+floatingnumber = 2.0
+# display
+print(f'floating value is {floatingnumber}')
+
+# variable that holds a boolean
+isValid = True
+# display
+print(f'boolean Value is {isValid}')
