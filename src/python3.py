6ac9ec29767095bb8077952ad84b48b866ef6e14
Adrian <ianxbautista06@yahoo.com>
2022-10-05 15:04:35 +0800

    commit python3

---
+++ src/python3.py
@@ -0,0 +1,18 @@
+# change variable value and checking of data type
+# create variable and assign 10
+x = 10
+
+# check data type
+print(type(x))
+
+# reassign from 10 to Hello
+x = "Hello"
+print(type(x))
+
+# reassign from hello to boolean true
+x = True
+print(type(x))
+
+# reassign from boolean True to float 3.14
+x = 3.14
+print(type(x))
