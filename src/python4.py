fbbdf0aaae7276220cc5f9a2741fd693e54bb4c1
Adrian <ianxbautista06@yahoo.com>
2022-10-05 15:11:25 +0800

    commit python4

---
+++ src/python4.py
@@ -0,0 +1,23 @@
+# arithmetic operators
+number1 = 6
+number2 = 3
+sum = number1 + number2
+
+# add
+print(f'the sum of {number1} and {number2} is {sum}')
+
+# subtract
+diff = number1 - number2
+print(f'the difference of {number1} and {number2} is {diff}')
+
+# product
+prod = number1 * number2
+print(f'the product of {number1} and {number2} is {prod}')
+
+# division
+quotient = number1 / number2
+print(f'the quotient of {number1} and {number2} is {quotient}')
+
+# modulo
+modulo = number1 % number2
+print(f'the remainder of {number1} and {number2} is {modulo}')
