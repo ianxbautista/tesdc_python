21e40c2660bff1749c4411494d3a06f389f628ba
Adrian <ianxbautista06@yahoo.com>
2022-10-05 15:15:43 +0800

    commit python5

---
+++ src/python5.py
@@ -0,0 +1,15 @@
+# conditional operators
+# returns boolean values true or false
+
+# greater than
+print(6>3)
+# lesser than
+print(6<3)
+# less than or equal
+print(3<=3)
+# greater than or equal
+print(3>=3)
+# equal to
+print(3==3)
+# not equal to
+print(3!=3)
